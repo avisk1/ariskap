05debd6619b6dbbcaff4757a7ce4b46f46a73ef6
avisk1 <arisk56@outlook.com>
2021-06-25 22:23:58 -0400

    mongo please work

--- server.js
+++ server.js
@@ -2,18 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 require('dotenv').config();
 
+console.log(process.env.TEST);
+console.log(process.env.USERNAME);
+
 //mongoose
-const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?retryWrites=true&w=majority&authSource=admin`;
+// const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authSource=admin`;
+const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authMechanism=SCRAM-SHA-1`;
+// const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authSource=admin`;
+
 
 mongoose.connect(dbURL)
   .then(() => {
-      console.log("wait did it actually work?")
+      console.log("wait did it actually work?");
   })
   .catch(err => {
       console.error('App starting error:', err);
   });
 
-// const Message = mongoose.model("Message",{ name : String, message : String})
+
+const Message = mongoose.model("messages",{ name : String, message : String})
+
+console.log(Message);
 
 //express
 const app = express();
