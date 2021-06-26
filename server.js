6654f8c3a80831eff84af11101b5d4fdc6fa7153
avisk1 <arisk56@outlook.com>
2021-06-26 11:18:32 -0400

    testing express requests

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const mongoose = require("mongoose");
+// const bodyParser = require("body-parser");
 require('dotenv').config();
 
 console.log(process.env.TEST);
 console.log(process.env.USERNAME);
 
 //mongoose
-// const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authSource=admin`;
-const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authMechanism=SCRAM-SHA-1`;
-// const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authSource=admin`;
+//not tried yet
+const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authSource=admin&w=1`;
+// const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authMechanism=SCRAM-SHA-1&w=1`;
+// const dbURL = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.xpx7r.mongodb.net/myFirstDatabase?authSource=admin&w=1`;
 
 
 mongoose.connect(dbURL)
@@ -27,13 +29,40 @@ console.log(Message);
 //express
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({extended: false}))
+
 // use the express-static middleware
 app.use(express.static("public"));
-
-// app.get('/', (req, res) => {
-//   return res.send('<h1>Hello World!</h1><script>console.log("Hello world!")</script>')
-// })
 
 // start the server listening for requests
 app.listen(process.env.PORT || 3000, 
 	() => console.log("Server is running..."));
+
+//when a get request is sent to /stream (wants information)
+app.get('/stream', (req, res) => {
+  //return message data from database
+  console.log("A GET request has been sent to /stream");
+  res.send("A GET request has been sent to /stream");
+
+  // Message.find({},(err, messages)=> {
+    // res.send(messages);
+  // })
+});
+
+//when a post request is sent to /stream (giving information)
+app.post('/stream', (req, res) => {
+  //posts this new information to the database
+  console.log("A POST request has been sent to /stream. Its contents are:");
+  console.log(req.body);
+  console.log(req.body.name);
+  console.log(req.body.message);
+
+  // var message = new Message(req.body);
+  // message.save((err) =>{
+    // if(err)
+      // sendStatus(500);
+    // res.sendStatus(200);
+  // })
+});
+
